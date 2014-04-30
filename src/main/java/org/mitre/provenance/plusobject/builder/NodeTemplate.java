50bbd8c05b87846859439d203e729d4a6ce8fbc4
Moxious <moxious@oldhat.org>
2014-04-30 15:54:09 -0400

    REFACTOR: moved class package

---
+++ src/main/java/org/mitre/provenance/plusobject/builder/NodeTemplate.java
@@ -0,0 +1,34 @@
+/* Copyright 2014 MITRE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mitre.provenance.plusobject.builder;
+
+/**
+ * Helper class to repetitively generate many DeclarativeBuilders based on a template.
+ * The name template should contain variable names in curly braces, e.g. "Data item {x}".
+ * With a template with that name, generate() can then be called with varName="x" and various values
+ * to generate novel data items. 
+ * @author moxious
+ */
+public class NodeTemplate {
+	public enum NodeType { DATA, INVOCATION, WORKFLOW };
+	
+	public NodeType nt;
+	public String name;
+	
+	public NodeTemplate(NodeType nt, String name) { 
+		this.nt = nt;
+		this.name = name;
+	}
+}
