b121bcacb9bf908e16b27dca795d0596fa0800f8
Moxious <moxious@oldhat.org>
2014-07-22 15:49:02 -0400

    added implemented methods for PC dominates checking, and PS checking; added abstract methods for client implementation

--- src/main/java/org/mitre/provenance/client/AbstractProvenanceClient.java
+++ src/main/java/org/mitre/provenance/client/AbstractProvenanceClient.java
@@ -15,13 +15,17 @@
 package org.mitre.provenance.client;
 
 import java.util.List;
+import java.util.ListIterator;
 
 import org.mitre.provenance.Metadata;
+import org.mitre.provenance.PLUSException;
 import org.mitre.provenance.dag.TraversalSettings;
 import org.mitre.provenance.plusobject.PLUSActor;
 import org.mitre.provenance.plusobject.PLUSObject;
 import org.mitre.provenance.plusobject.PLUSWorkflow;
 import org.mitre.provenance.plusobject.ProvenanceCollection;
+import org.mitre.provenance.user.PrivilegeClass;
+import org.mitre.provenance.user.PrivilegeSet;
 import org.mitre.provenance.user.User;
 
 /**
@@ -160,4 +164,123 @@ public abstract class AbstractProvenanceClient {
 	 * @throws ProvenanceClientException
 	 */
 	public abstract ProvenanceCollection search(Metadata parameters, int max) throws ProvenanceClientException;
+	
+	/**
+	 * Indicate whether this set of privileges dominates a particular class.  If a member of this set dominates a particular
+	 * class, then the user possessing that set is authorized to see data at that privilege class.   
+	 * @param c the class to check
+	 * @return true if any privilege class in the set dominates c
+	 * @throws PLUSException
+	 */
+	public boolean dominates(PrivilegeSet ps, PrivilegeClass other) throws ProvenanceClientException {
+		for(int x=0; x<ps.set.size(); x++) { 
+			PrivilegeClass p = ps.set.get(x);
+			if(dominates(p, other)) return true;
+		}
+			
+		return false;
+	}
+	
+	/**
+	 * Determine whether this privilege set dominates another.
+	 * @param one the first set to check.
+	 * @param other the other you want to check.
+	 * @return true if one privilege set contains a set of privilege classes that dominates all privilege classes in the
+	 * other privilege set, false otherwise.  In the case of incomparable PrivilegeSets, this method will return 
+	 * false.
+	 * @throws PLUSException
+	 */
+	public boolean dominates(PrivilegeSet one, PrivilegeSet other) throws ProvenanceClientException { 
+		try {
+			ListIterator <PrivilegeClass> otherprivileges = (other.getPrivilegeSet()).listIterator();
+			while (otherprivileges.hasNext()) {
+				PrivilegeClass otherprivilege = otherprivileges.next();
+				if (!dominates(one, otherprivilege)) 	
+					return false;
+			} // End while
+			
+			return true;
+		} catch(PLUSException e) { 
+			throw new ProvenanceClientException(e); 
+		} // End catch
+	} // End dominates(PrivilegeSet)
+	
+	public abstract boolean dominates(PrivilegeClass a, PrivilegeClass b) throws ProvenanceClientException;
+	
+	/**
+	 * Determine whether this user can see information at privilege class p.
+	 * @param user the user
+	 * @param p the class you want to check
+	 * @return true if the user has this privilege, or has a privilege which dominates it.
+	 */
+	public boolean authorizedFor(User user, PrivilegeClass p) throws ProvenanceClientException {
+		if(user == User.DEFAULT_USER_GOD) return true;
+		return dominates(user.getPrivileges(), p);
+	}
+		
+	/**
+	 * @param obj
+	 * @param conjunctive
+	 * @return
+	 * @throws PLUSException
+	 */
+	public boolean canSee(User user, PLUSObject obj, boolean conjunctive) throws PLUSException {
+		try {
+			if (conjunctive) return canSee(user, obj);
+			return canSeeDisjunctive(user, obj);
+		} catch(PLUSException e) { 
+			throw new PLUSException("PrivilegeSet#dominatesDisjunctive: " + e, e); 
+		}
+	}	
+	
+	/**
+	 * Check and see if a given PLUSObject should be visible to this user.
+	 * @param user the user in question
+	 * @param obj the object you want to check.
+	 * @return true if the user's privileges dominate ALL of the privileges required by the object, false otherwise.
+	 * @throws PLUSException
+	 */
+	public boolean canSee(User user, PLUSObject obj) throws ProvenanceClientException {
+		if(user == User.DEFAULT_USER_GOD) return true;
+		if(user.getPrivileges().contains(PrivilegeClass.ADMIN)) return true;
+		
+		PrivilegeSet ps = obj.getPrivileges();
+		
+		// Check each privilege class in the object.
+		// If the user isn't authorized for one of them, they can't see this object.
+		for(int x=0; x<ps.set.size(); x++) { 
+			PrivilegeClass c = ps.set.get(x);
+					
+			if(!authorizedFor(user, c)) {
+				// PLUSUtils.log.debug("Not authorized for " + c); 
+				return false;
+			} // else System.out.print(c + " (check), ");
+		} // End for
+		
+		return true;
+	}	
+	
+	/**
+	 * Performs a disjunctive check to see whether a user can see the object in question.  
+	 * @param user
+	 * @param obj
+	 * @return true if the user is authorized for ANY ONE of the privileges required by the object (not necessarily all)
+	 */
+	private boolean canSeeDisjunctive(User user, PLUSObject obj) throws PLUSException {		
+		if(user == User.DEFAULT_USER_GOD) return true;
+		if(user.getPrivileges().contains(PrivilegeClass.ADMIN)) return true; 
+		
+		PrivilegeSet ps = obj.getPrivileges();
+		
+		// Check each privilege class in the object.
+		// If the user is authorized for one of them, they can see this object.
+		for(int x=0; x<ps.set.size(); x++) { 
+			PrivilegeClass c = ps.set.get(x);
+			if(authorizedFor(user, c)) {
+				return true;
+			} // else System.out.print(c + " (check), ");
+		} // End for
+
+		return false;
+	}	
 } // End AbstractProvenanceClient
