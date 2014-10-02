75e4f1a623984f275f395bb19f75df100fb55f07
Moxious <moxious@oldhat.org>
2014-10-02 17:02:16 -0400

    refactored to add extractMetadata() removing it from PLUSFile

--- src/main/java/org/mitre/provenance/tools/FileHasher.java
+++ src/main/java/org/mitre/provenance/tools/FileHasher.java
@@ -16,19 +16,74 @@ package org.mitre.provenance.tools;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.UserPrincipal;
+import java.util.logging.Logger;
 
+import org.mitre.provenance.PLUSException;
+import org.mitre.provenance.client.AbstractProvenanceClient;
 import org.mitre.provenance.client.LocalProvenanceClient;
+import org.mitre.provenance.db.neo4j.Neo4JPLUSObjectFactory;
 import org.mitre.provenance.npe.NonProvenanceEdge;
+import org.mitre.provenance.plusobject.PLUSActor;
 import org.mitre.provenance.plusobject.PLUSFile;
 import org.mitre.provenance.plusobject.ProvenanceCollection;
 
 /**
- * A simple tool to ease hashing of local files.
+ * A simple tool to ease hashing/extraction of metadata of local files.
  * @author DMALLEN
  */
-public class FileHasher {	
+public class FileHasher {		
+	protected static final Logger log = Logger.getLogger(FileHasher.class.getName());
+	
+	/**
+	 * Given a file object, this creates a PLUSFile representation filled with appropriate metadata, file owner, and so on.
+	 * @param file
+	 * @return
+	 */
+	public static PLUSFile extractMetadata(File file, AbstractProvenanceClient client) { 
+		if(!file.exists() || !file.canRead()) return null;
+		
+		PLUSFile f = new PLUSFile(file);
+		
+		// Try to figure out who owns this file, if it exists.
+		if(f.getOwner() == null) {
+			// This is a java-7 ism that permits us to access the file owner.
+			try { 
+				Path path = Paths.get(file.getAbsolutePath());
+				UserPrincipal owner = Files.getOwner(path);
+				String username = owner.getName();
+				
+				try {
+					PLUSActor a = client.actorExistsByName(username);
+					if(a == null) a = new PLUSActor(username);
+					
+					f.setOwner(a);
+				} catch (PLUSException e) {
+					log.warning(e.getMessage());
+				}
+			} catch(IOException exc) { 
+				log.warning(exc.getMessage());
+			}
+		}
+		
+		try { f.getMetadata().put("isLink", ""+file.getCanonicalFile().equals(file.getAbsoluteFile())); } catch(Exception exc) { ; } 
+		try { f.getMetadata().put("exists", ""+file.exists()); } catch(Exception exc) { ; }
+		try { f.getMetadata().put("path", file.getAbsolutePath()); } catch(Exception exc){ ; }
+		try { f.getMetadata().put("canonical", file.getCanonicalPath()); } catch(Exception exc) { ; } 
+		try { f.getMetadata().put("directory", ""+file.isDirectory()); } catch(Exception exc) { ; } 
+		try { f.getMetadata().put("lastmodified", ""+file.lastModified()); } catch(Exception exc) { ; }		
+		try { f.getMetadata().put("size", ""+file.length()); } catch(Exception exc) { ; } 
+		
+		return f;
+	} // End extractMetadata
+
+	
 	/**
 	 * Takes a list of files as arguments; hashes each file, and creates a corresponding PLUSFile provenance object
 	 * in the local repository.  Creates the corresponding hash metadata entry and an NPE to the hash value. 
