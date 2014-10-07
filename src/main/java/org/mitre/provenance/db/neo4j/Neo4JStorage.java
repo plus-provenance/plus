9242e403281e95b961a5a425764fe9a71f4a3c39
Moxious <moxious@oldhat.org>
2014-10-07 15:50:43 -0400

    fixed bugged query with incorrect cypher syntax

--- src/main/java/org/mitre/provenance/db/neo4j/Neo4JStorage.java
+++ src/main/java/org/mitre/provenance/db/neo4j/Neo4JStorage.java
@@ -818,7 +818,7 @@ public class Neo4JStorage {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put( "name", name );
 
-		String query = "match (a:Actor {name={name}}) return a";
+		String query = "match (a:Actor {name: {name}}) return a";
 		
 		ExecutionResult result = execute(query, params );
 				
